30d19101ef10669f8c74ee0014406991abd99831
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-09-05 14:57:43 -0400

    add update and destroy to products show

--- src/Content.jsx
+++ src/Content.jsx
@@ -30,12 +30,36 @@ const handleClose = () => {
 }
 
 const handleCreateProduct = (params, successCallback) => {
-       console.log("handleCreateProduct", params);
        axios.post("http://localhost:3000/products.json", params).then((response) => {
          setProducts([...products, response.data]);
          successCallback();
        });
      };
+
+const handleUpdateProduct = (id, params, successCallback) => {
+           console.log("handleUpdateProduct", params);
+        axios.patch(`http://localhost:3000/products/${id}.json`, params).then((response) => {
+             setProducts(
+               products.map((product) => {
+                 if (product.id === response.data.id) {
+                   return response.data;
+                 } else {
+                   return product;
+                 }
+               })
+             );
+             successCallback();
+             handleClose();
+           });
+         };
+
+const handleDestroyProduct = (product) => {
+               console.log("handleDestroyProduct", product);
+               axios.delete(`http://localhost:3000/products/${product.id}.json`).then((response) => {
+                 setProducts(products.filter((p) => p.id !== product.id));
+                 handleClose();
+               });
+             };
 
 useEffect(getProducts, [])
   return (
@@ -47,7 +71,7 @@ useEffect(getProducts, [])
       <ProductsNew onCreateProduct={handleCreateProduct}/>
       <ProductsIndex products={products} onShowProduct={handleShowProduct}/>
       <Modal show={isProductsShowVisible} onClose={handleClose}> 
-      <ProductsShow product={currentProduct}/>
+      <ProductsShow product={currentProduct} onUpdateProduct={handleUpdateProduct} onDestroyProduct={handleDestroyProduct}/>
       </Modal>
     </div>
   )
--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -1,12 +1,39 @@
 export function ProductsShow(props) {
-  console.log(props.product)
-  return (
+     const handleSubmit = (event) => {
+         event.preventDefault();
+         const params = new FormData(event.target);
+         props.onUpdateProduct(props.product.id, params, () => event.target.reset());
+       };
+
+      const handleClick = () => {
+props.onDestroyProduct(props.product)
+      } 
+
+      return (
     <div>
       <h1>Product information</h1>
       <p>Name: {props.product.name}</p>
       {/* <p>Url: {props.product.url}</p> */}
       <p>Description: {props.product.description}</p>
       <p>Price: {props.product.price}</p>
+
+      <form onSubmit={handleSubmit}>
+         <div>
+           Name: <input defaultValue={props.product.name} name="name" type="text" />
+         </div>
+         <div>
+           Description: <input defaultValue={props.product.description} name="description" type="text" />
+         </div>
+         <div>
+           Price: <input defaultValue={props.product.price} name="price" type="text" />
+         </div>
+         <div>
+           Supplier: <input defaultValue={props.product.supplier_id} name="supplier_id" type="text" />
+         </div>
+         <button type="submit">Update product</button>
+       </form>
+<br></br>
+       <button onClick={handleClick}>Delete product</button>
     </div>
   );
 }

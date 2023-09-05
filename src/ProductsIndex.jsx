b16c4b6044a004b05521a832aa7a0b0694208e95
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-09-05 15:51:46 -0400

    add 2 column index

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,8 +1,10 @@
 export function ProductsIndex(props) {
   return(
     <div id="posts-index">
+       <div className="row row-cols-2">
      {props.products.map(product => (
       <div key={product.id}>
+        <div className="col">
          <div className="card">
             <div className="card-body">
       <h1>{product.name}</h1>
@@ -10,11 +12,13 @@ export function ProductsIndex(props) {
       <p>Description: {product.description}</p>
       <p>Price: ${product.price}</p>
       <button onClick={() => props.onShowProduct(product)}>More info</button>
+      </div>
       </div>   
       </div>   
       <br></br>
       </div>
      ))}
+     </div>
     </div>
   )
 }
